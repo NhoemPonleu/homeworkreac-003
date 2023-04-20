9a991e6835fad9d10aa251906ac9a389569b3c69
nhoem.ponleu <ponleunhoem@gmail.com>
2023-04-20 18:34:57 +0700

    react

---
+++ homework-003/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Products from './component/Products';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div >
+      <div className='text'>
+        <h1>Lim Dane From Class Dev ops</h1>
+      </div>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
